c2807908c2302e7002c1b320bd20a8375699e943
kyalo460 <kyalo460@gmail.com>
2023-07-22 17:07:54 +0000

    First commit with function

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+/**
+  *printf.h - this file will contain our prototypes
+  *_printf - variadic function
+  *@format: the format specifiers
+  *
+  *Return: returns something
+  */
+int _printf(const char *format, ...);
+#endif
---
+++ test.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+int _printf(const char *format, ...)
+{
+	int i = 0, len = 0;
+	char char_,char_format ;
+	char * string;
+	va_list arg;
+	va_start(arg, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			if (format[i] =='%')
+			{
+				i++;
+				if (format[i] == 'c')
+				{
+					char_ = va_arg(arg, int);
+					write(1, &char_, 1);
+					len++;
+				}
+
+				else if (format[i] == 's')
+				{
+					string = va_arg(arg, char *);
+					write(1, string, strlen(string));
+					len = len + strlen(string);
+				}
+				i++;
+			}
+
+			if (format[i] == '%')
+				continue;
+
+			char_format = format[i];
+
+			if(char_format == '\0')
+				break;
+
+			write(1, &char_format, 1); 
+			i++;
+			len++;
+		}
+	}
+	va_end(arg);
+	return (len);
+}
