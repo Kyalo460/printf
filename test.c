7531f5811a47adc3d9dd0ddd2ad3ffc87e46a3b0
kyalo460 <kyalo460@gmail.com>
2023-07-23 08:56:12 +0000

    Added more conditions

--- test.c
+++ test.c
@@ -5,51 +5,102 @@
 #include "main.h"
 
 /**
- * _printf - Replica of printf funtion
- * @format: contains normal characters & format specifiers
- *
- * Return: Length of String
- */
+  *print_string - prints a string
+  *@len: length of string
+  *@arg: list of variadic arguments
+  *
+  *Return: returns the length (if success)
+  */
+int print_string(int len, va_list arg)
+{
+	char *string;
+
+	string = va_arg(arg, char *);
+
+	if (string == NULL)
+		string = "(NULL)";
+
+	write(1, string, strlen(string));
+	len += strlen(string);
+	return (len);
+
+}
+/**
+  *print_char- prints a character
+  *@len: length of string
+  *@arg: list of variadic arguments
+  *
+  *Return: returns the length (if success)
+  */
+int print_char(int len, va_list arg)
+{
+	char char_;
+
+	char_ = va_arg(arg, int);
+
+	write(1, &char_, 1);
+	len++;
+	return (len);
+}
+/**
+  *print_perc - prints %
+  *@len: length of string
+  *
+  *Return: returns the length (if success)
+  */
+int print_perc(int len)
+{
+	char char_ = '%';
+
+	write(1, &char_, 1);
+	len++;
+	return (len);
+}
+/**
+  *_printf - Replica of printf funtion
+  *@format: contains normal characters & format specifiers
+  *
+  *Return: Length of String
+  */
 int _printf(const char *format, ...)
 {
 	int i = 0, len = 0;
-	char char_, char_format;
-	char *string;
+	char char_format;
+	/*char *string;*/
 	va_list arg;
 
 	va_start(arg, format);
-	if (format)
+	if (format != NULL)
 	{
-		while (format[i])
+		for (i = 0; format[i]; i++)
 		{
 			if (format[i] == '%')
 			{
 				i++;
 				if (format[i] == 'c')
-				{
-					char_ = va_arg(arg, int);
-					write(1, &char_, 1);
-					len++;
-				}
+					len = print_char(len, arg);
+
 				else if (format[i] == 's')
-				{
-					string = va_arg(arg, char *);
-					write(1, string, strlen(string));
-					len = len + strlen(string);
-				}
+					len = print_string(len, arg);
+
+				else if (format[i] == '%')
+					len = print_perc(len);
+
+				else
+					return (-1);
+
 				i++;
 			}
-			else
-				return (-1);
 			if (format[i] == '%')
 				continue;
 			char_format = format[i];
 			if (char_format == '\0')
 				break;
 			write(1, &char_format, 1);
-			i++;
 			len++;
 		}
 	}
+	else
+		return (-1);
 	return (len);
 }
