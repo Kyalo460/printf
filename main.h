5bd98d5e44d3e80854953a8f37552d8dbf280656
kyalo460 <kyalo460@gmail.com>
2023-07-24 15:40:20 +0000

    Updated header file

--- main.h
+++ main.h
@@ -1,5 +1,9 @@
 #ifndef PRINTF_H
 #define PRINTF_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
 /**
   *printf.h - this file will contain our prototypes
   *_printf - variadic function
@@ -8,4 +12,8 @@
   *Return: returns something
   */
 int _printf(const char *format, ...);
+int print_string(int len, va_list arg);
+int print_char(int len, va_list arg);
+int print_perc(int len);
+int check_char(char format, int len, va_list arg);
 #endif
