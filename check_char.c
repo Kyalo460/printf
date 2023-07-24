333d1ffce77971913573c6f2392586fab9728c5c
kyalo460 <kyalo460@gmail.com>
2023-07-24 16:00:05 +0000

    check_char.c function file

---
+++ check_char.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ *check_char - checks if character is special
+ *@format: contains a character
+ *@len: length of output
+ *@arg: list of variadic arguments
+ *
+ *Return: Length of String (if success)
+ */
+int check_char(char format, int len, va_list arg)
+{
+	if (format == 'c')
+	{
+		len = print_char(len, arg);
+		return (len);
+	}
+
+	else if (format == 's')
+	{
+		len = print_string(len, arg);
+		return (len);
+	}
+
+	else if (format == '%')
+	{
+		len = print_perc(len);
+		return (len);
+	}
+
+	else if (format == ' ')
+		return (-1);
+
+	else if (format == '\0')
+		return (-1);
+
+	else
+	{
+		len = print_perc(len);
+		write(1, &format, 1);
+		len++;
+	}
+
+	return (len);
+}
